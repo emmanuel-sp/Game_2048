773bdef8326349e26e25e7c47190d462951de3f2
emmanuel-sp <esp48321@uga.edu>
2023-06-14 22:35:54 -0400

    *Added game logic *Made gui functional

--- game/twenty48.py
+++ game/twenty48.py
@@ -1,9 +1,83 @@
 # twenty48.py
-
+import time
 class Twenty48:
     def __init__(self):
-        # Initialize game state
         self.score = 0
-        # Add more game logic as needed
+        self.board = [[0 for _ in range(4)] for _ in range(4)]
 
+    def push(self, direction):
+        for i in range(3):
+            if direction == "Up":       
+                for row in range(1,4):   
+                    for tile in self.board[row]:
+                        if tile == 0:
+                            continue
+                        elif tile == self.board[row - 1][self.board[row].index(tile)]:
+                            self.board[row - 1][self.board[row].index(tile)] *= 2
+                            self.board[row][self.board[row].index(tile)] = 0
+                            #self.score += self.board[row - 1][self.board[row].index(tile)]
+                        elif self.board[row - 1][self.board[row].index(tile)] == 0:
+                            self.board[row - 1][self.board[row].index(tile)] = tile
+                            self.board[row][self.board[row].index(tile)] = 0
+            elif direction == "Down":
+                for row in range(2, -1, -1):   
+                    for tile in self.board[row]:
+                        if tile == 0:
+                            continue
+                        elif tile == self.board[row + 1][self.board[row].index(tile)]:
+                            self.board[row + 1][self.board[row].index(tile)] *= 2
+                            self.board[row][self.board[row].index(tile)] = 0
+                            #self.score += self.board[row + 1][self.board[row].index(tile)]
+                        elif self.board[row + 1][self.board[row].index(tile)] == 0:
+                            self.board[row + 1][self.board[row].index(tile)] = tile
+                            self.board[row][self.board[row].index(tile)] = 0
+            elif direction == "Left":
+                for col in range(1,4):   
+                    for tile in self.board:
+                        if tile[col] == 0:
+                            continue
+                        elif tile[col] == tile[col - 1]:
+                            tile[col - 1] *= 2
+                            tile[col] = 0
+                            #self.score += tile[col - 1]
+                        elif tile[col - 1] == 0:
+                            print(True)
+                            tile[col - 1] = tile[col]
+                            tile[col] = 0
+            elif direction == "Right":
+                for col in range(2, -1, -1):   
+                    for tile in self.board:
+                        if tile[col] == 0:
+                            print(True)
+                            continue
+                        elif tile[col] == tile[col + 1]:
+                            print(True)
+                            tile[col + 1] *= 2
+                            tile[col] = 0
+                            #self.score += tile[col + 1]
+                        elif tile[col + 1] == 0:
+                            print(True)
+                            tile[col + 1] = tile[col]
+                            tile[col] = 0
+        print(self.board)
+        
+
+    def setNumber(self, row, col, number):
+        self.board[row][col] = number
     
+    def getNumber(self, row, col):
+        return self.board[row][col]
+    
+    def addRandom(self):
+        pass
+
+    def isFull(self):
+        for row in self.board:
+            for col in row:
+                if col == 0:
+                    return False
+        return True
+
+
+
+print(Twenty48().board)    
--- game/window.py
+++ game/window.py
@@ -1,41 +1,80 @@
 # window.py
-
+from twenty48 import Twenty48 as t48
+import math
+import random
 from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QGridLayout, QWidget, QVBoxLayout, QGraphicsView, QGraphicsScene, QGraphicsRectItem
-from PyQt6.QtGui import QIcon, QColor, QFont, QFontMetrics
-from PyQt6.QtCore import  QSize, Qt, QRect, QRectF, QPointF
+from PyQt6.QtGui import QIcon, QColor, QFont, QFontMetrics, QKeyEvent
+from PyQt6.QtCore import  QSize, Qt, QRect, QRectF, QPointF, QEvent
 
-font = QFont("Clear Sans", 35, QFont.Weight.Bold)
+font_name = ""
+font = QFont(font_name, 35, QFont.Weight.Bold)
 class GraphicsScene(QGraphicsScene):
     def __init__(self):
         super().__init__()
         self.setBackgroundBrush(QColor(255,240,232,255))
         self.setSceneRect(0, 0, 550, 630) # Set the scene rectangle size
-        rectangles = []
-        texts = []
-        title_text = self.addSimpleText("2048", QFont("Clear Sans", 50, QFont.Weight.Bold))
-        title_text.setPos(10, 2)
-        title_text.setBrush(QColor(119,110,101,255))
-        signature = self.addSimpleText("Created by: Emmanuel Pierre", QFont("Clear Sans", 16))
-        signature.setPos(335, 605)
+        self.rectangles = []
+        self.texts = []
+        self.title_text = self.addSimpleText("2048", QFont("", 50, QFont.Weight.Bold))
+        self.title_text.setPos(10, 2)
+        self.title_text.setBrush(QColor(119,110,101,255))
+        self.signature = self.addSimpleText("Created by: Emmanuel Pierre", QFont(font_name, 16))
+        self.signature.setPos(335, 605)
         self.addRect(QRectF(0, 0, 520, 540), QColor(187,173,160,255), QColor(187,173,160,255)).setPos(14, 62)
-        font_metrics = QFontMetrics(font)
+        self.font_metrics = QFontMetrics(font)
+        self.game = t48()
+        self.game.board[random.randint(0,3)][random.randint(0,3)] = 2
 
         for row in range(4):
-            rectangles.append([])
-            texts.append([])
+            self.rectangles.append([])
+            self.texts.append([])
             for col in range(4):
-                text = "2" if row == 0 and col == 0 else ""
-                text_width = font_metrics.horizontalAdvance(text)  # Get the width of the text
-                text_height = font_metrics.height() 
-                rectangles[row].append(self.addRect(QRectF(0, 0, 115, 115), QColor(228,221,214,70), QColor(228,221,214,70)))
-                texts[row].append(self.addSimpleText(text, font))
-                rectangles[row][col].setPos(130 * col + 21.5, 130 * row + 80)
-                texts[row][col].setPos(rectangles[row][col].x() + 115 / 2 - text_width / 2, rectangles[row][col].y() + 115 / 2 - text_height / 2)
-                texts[row][col].setBrush(QColor(119,110,101,255))
+                text = ""
+                text_width = self.font_metrics.horizontalAdvance(text)  # Get the width of the text
+                text_height = self.font_metrics.height() 
+                self.rectangles[row].append(self.addRect(QRectF(0, 0, 115, 115), QColor(228,221,214,70), QColor(228,221,214,70)))
+                self.texts[row].append(self.addSimpleText(text, font))
+                self.rectangles[row][col].setPos(130 * col + 21.5, 130 * row + 80)
+                self.texts[row][col].setPos(self.rectangles[row][col].x() + 115 / 2 - text_width / 2, self.rectangles[row][col].y() + 115 / 2 - text_height / 2)
+                self.texts[row][col].setBrush(QColor(119,110,101,255))
+        self.updateboard()
 
-        rectangles[0][0].setBrush(QColor(238,228,218,255))
-        print(rectangles[0][0].scenePos())
-        print(rectangles[0][0].pos())
+    def updateboard(self):
+        for row in range(4):
+            for col in range(4):
+                num = self.game.board[row][col]
+                text = str(self.game.board[row][col])
+                if text == "0":
+                    text = ""
+                text_width = self.font_metrics.horizontalAdvance(text)  # Get the width of the text
+                text_height = self.font_metrics.height() 
+                self.texts[row][col].setText(text)
+                self.texts[row][col].setPos(self.rectangles[row][col].x() + 115 / 2 - text_width / 2, self.rectangles[row][col].y() + 115 / 2 - text_height / 2)
+                if num > 0: self.rectangles[row][col].setBrush(QColor(180, 150 + int((math.log(2, num) * 10)), 150 + int((math.log(2,num) * 10)), 255))
+                if num == 0: self.rectangles[row][col].setBrush(QColor(228,221,214,70))
+        
+
+    def keyPressEvent(self, event: QKeyEvent) -> None:
+        valid_keys = [Qt.Key.Key_Up, Qt.Key.Key_Down, Qt.Key.Key_Left, Qt.Key.Key_Right]
+        if event.key() == Qt.Key.Key_Up:
+            self.game.push("Up")
+        elif event.key() == Qt.Key.Key_Down:
+            self.game.push("Down")
+        elif event.key() == Qt.Key.Key_Left:
+            self.game.push("Left")
+        elif event.key() == Qt.Key.Key_Right:
+            self.game.push("Right")
+        if event.key() in valid_keys:
+            self.setRandomIndex()
+            self.updateboard()
+
+    def setRandomIndex(self):
+        random_x = random.randint(0,3)
+        random_y = random.randint(0,3)
+        if self.game.board[random_x][random_y] != 0:
+            self.setRandomIndex()
+        else:
+            self.game.board[random_x][random_y] = 2
 
 
 
